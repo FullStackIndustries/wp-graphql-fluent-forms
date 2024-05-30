3196872495b37210a5f2bf17c67d6cbd4d79838a
Sam Alexander <samalexander@users.noreply.github.com>
2024-05-30 11:14:24 +0100

    initial commit

---
+++ src/CoreSchemaFilters.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Adds filters that modify core schema.
+ *
+ * @package WPGraphQL\PluginName
+ */
+
+namespace WPGraphQL\PluginName;
+
+use WPGraphQL\PluginName\Vendor\AxeWP\GraphQL\Interfaces\Registrable;
+
+/**
+ * Class - CoreSchemaFilters
+ */
+class CoreSchemaFilters implements Registrable {
+	/**
+	 * {@inheritDoc}
+	 */
+	public static function init(): void {
+		add_filter( 'graphql_pb_type_prefix', [ self::class, 'get_type_prefix' ] );
+	}
+
+	/**
+	 * Prefixes all plugin GraphQL types.
+	 *
+	 * @param string $type_name the non-prefixed type name.
+	 */
+	public static function get_type_prefix( ?string $type_name = null ): string {
+		return ! empty( $type_name ) ? $type_name : 'PluginName';
+	}
+}
